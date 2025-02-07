808701450ddb9eeb3b64a794d2c880ac22b0ee6f
Stephan Guingor <fanzy@Fanzys-MacBook-Air.local>
2025-02-07 12:54:26 -0600

    [add] create amp cart on blind pick

--- src/jokers/jokers1.lua
+++ src/jokers/jokers1.lua
@@ -3,19 +3,33 @@ local gambler ={
     name = "gambler",
     pos = {x = 0, y = 0},
     config = {extra = {h_size = 1}},
-    rarity = 2, 
-    cost = 5, 
+    rarity = 1, 
+    cost = 1, 
     atlas = "jokers",
     blueprint_compat = true,
     unlocked = true,
     discovered = true,
     calculate = function(self, card, context)
-     
+     if context.setting_blind and not self.getting_sliced then
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, nil, nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+                return true
+            end
+        }))
+
+        card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil,
+				{ message = localize('k_duplicated_ex') })
+     end
     end,
     add_to_deck = function(self, card, from_debuff)
       if not from_debuff then
         G.hand:change_size(card.ability.extra.h_size)
       end
+
+
     end,
     remove_from_deck = function(self, card, from_debuff)
       if not from_debuff then
