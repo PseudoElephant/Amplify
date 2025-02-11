3320698ef69b3457195b0237e0fb4a3ed410b5bc
AlePPisa <aleppaulon@gmail.com>
2025-02-10 21:41:14 -0800

    Change Amplify Deck to start with all amplify cards for debug purposes. Modify gambler joker to get all modify cards at start of round

--- src/backs/backs1.lua
+++ src/backs/backs1.lua
@@ -5,12 +5,34 @@ local amplifydeck = {
 	key = "amplifydeck",  
 	name = "amplifydeck",
 	order = 50,
-  	unlocked = true,
-  	discovered = true,
-	config = {consumables = {'c_amp_radio', 'c_amp_tuning_fork'}, dollars=10000},
+	unlocked = true,
+	discovered = true,
+	config = {
+		consumables = {
+			"c_amp_radio", 
+			"c_amp_speaker", 
+			"c_amp_headphones", 
+			"c_amp_antenna", 
+			"c_amp_sonar", 
+			"c_amp_satellite_dish", 
+			"c_amp_megaphone", 
+			"c_amp_microphone", 
+			"c_amp_oscillator", 
+			"c_amp_record_player", 
+			"c_amp_soundboard", 
+			"c_amp_tape_recorder", 
+			"c_amp_echo", 
+			"c_amp_equalizer", 
+			"c_amp_amplified", 
+			"c_amp_tuning_fork"
+		}, 
+		dollars=10000
+	},
+
   loc_vars = function(self, info_queue, center)
     return {vars = {}}
   end,
+
 	pos = { x = 0, y = 0 },
 	atlas = "amplifydecks"
 }
--- src/jokers/jokers1.lua
+++ src/jokers/jokers1.lua
@@ -13,9 +13,70 @@ local gambler ={
      if context.setting_blind and not self.getting_sliced then
         G.E_MANAGER:add_event(Event({
             func = function()
-                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, nil, nil)
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_radio", nil)
                 card:add_to_deck()
                 G.consumeables:emplace(card)
+                
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_speaker", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_headphones", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_antenna", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_sonar", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_satellite_dish", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_megaphone", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_microphone", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_oscillator", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_record_player", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_soundboard", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_tape_recorder", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_echo", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_equalizer", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_amplified", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
+                local card = create_card("Amplify", G.pack_cards, nil, nil, true, true, "c_amp_tuning_fork", nil)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+
                 return true
             end
         }))
